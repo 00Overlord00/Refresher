f280edba7435e629bd784204ad21f2f1588145f6
00Overlord00 <nick.okey@mchsi.com>
2016-10-13 20:10:35 -0500

    Server up and running...

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ var pg = require( 'pg' );
 var bodyParser = require( 'body-parser' );
 var urlencodedParser = bodyParser.urlencoded( { extended: false } );
 
-var connectionString = 'postgres//localhost:5432/........'; //<---------DB name here.
+var connectionString = 'postgres//localhost:5432/testDB';
+
+app.use( bodyParser.json() );
 
 app.listen( 9000, 'localhost', function( req, res ) {
   console.log( 'Hailing frequencies open, Captain. Listening on PORT 5500.' );
@@ -15,4 +17,4 @@ app.use( express.static( 'public' ) );
 
 app.get( '/', function( req, res ) {
   res.sendFile( path.resolve( 'public/views/index.html' ) );
-})
+});
